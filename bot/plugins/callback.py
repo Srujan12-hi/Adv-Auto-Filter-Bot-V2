3314dce52e11741a2fff9968e900ed10ce5e1019
Srujan12-hi <85944675+Srujan12-hi@users.noreply.github.com>
2021-08-21 17:51:00 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -1601,14 +1601,14 @@ async def cb_about(bot, update: CallbackQuery):
     text=f"<i><u>Bot's Status</u></i>\n"
     text+=f"\n<b><i>Bot's Uptime:</i></b> <code>{time_formatter(time.time() - start_uptime)}</code>\n"
     text+=f"\n<b><i>Bot Funtion:</i></b> <i>Auto Filter Files</i>\n"
-    text+=f"""\n<b><i>Bot Support:</i></b> <a href="https://t.me/CrazyBotszGrp">@CrazyBotszGrp</a>\n"""
-    text+="""\n<b><i>Source Code:</i></b> <a href="https://github.com/CrazyBotsz/Adv-Filter-Bot-V2">Source</a>"""
+    text+=f"""\n<b><i>Support Group:</i></b> <a href="https://t.me/joinchat/qot4N1ipUcRhZDVl">Radha krishna katha</a>\n"""
+    text+="""\n<b><i>Source Code:</i></b> <a href="https://t.me/SOURCE_CODE_OF_FILE_STORE_BOT/2">Source</a>"""
 
     buttons = [
         [
             InlineKeyboardButton
                 (
-                    "My Dev ⚡", url="https://t.me/AlbertEinstein_TG"
+                    "My Dev ⚡", url="https://t.me/Sruja_12"
                 ),
                 
             InlineKeyboardButton
@@ -1639,13 +1639,11 @@ async def callback_data(bot, update: CallbackQuery):
 
     if query_data == "start":
         buttons = [[
-            InlineKeyboardButton('My Dev 👨‍🔬', url='https://t.me/AlbertEinstein_TG'),
-            InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Filter-Bot-V2')
-        ],[
-            InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
-        ],[
-            InlineKeyboardButton('Help ⚙', callback_data="help")
+            InlineKeyboardButton('My Dev 👨‍🔬', url='https://t.me/Sruja_12'),
+            InlineKeyboardButton('Main Channel', url ='https://t.me/Rkrishnaa)
         ]]
+
+
     
         reply_markup = InlineKeyboardMarkup(buttons)
         
